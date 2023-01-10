223cd3f48a52f2d7b02aa40da64d05ddf7ab7e56
Neto study <daria.bugaevskaya@gmail.com>
2023-01-10 14:49:06 +0100

    Methods added

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -11,5 +11,34 @@ public class Radio {
     private int currentVolume;
     private final int maxVolume = 10;
     private final int minVolume = 0;
+
+    public void nextStation() {
+        if (currentStation < 9) {
+            currentStation += 1;
+        } else {
+            currentStation = 0;
+        }
+    }
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation -= 1;
+        } else {
+            currentStation = 9;
+        }
+    }
+    public void higherVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume += 1;
+        } else {
+            currentVolume = maxVolume;
+        }
+    }
+    public void lowerVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume -= 1;
+        } else {
+            currentVolume = minVolume;
+        }
+    }
 }
 
